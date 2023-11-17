4342819a06c64725f62220756e545bc073cb3743
Fran <fducekuce@gmail.com>
2023-11-17 19:09:15 +0100

    Update CV

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -42,6 +42,7 @@ export default function Form({ secret }: { secret: string }) {
   return (
     <>
       <form method="POST" onSubmit={handleSubmit}>
+        <h1 className="text-black">{secret}</h1>
         <div className="mb-4">
           <label
             htmlFor="name"
